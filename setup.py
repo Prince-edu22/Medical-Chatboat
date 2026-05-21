5b6d0d4665f5084f4c537f24eb0c0c36b655649b
Prince Raj <rajprince6976@gmail.com>
2026-05-21 16:34:50 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatboat",
+    version="0.1.0",
+    author="prince",
+    author_email="rajprince6976@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
